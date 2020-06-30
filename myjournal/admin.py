07381289ba2087ec55b4ed0558db15a71f63fe37
kibet <kmishmael@gmail.com>
2020-06-30 13:46:38 +0300

    myjournal model deleted for now.

--- myjournal/admin.py
+++ myjournal/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 
-from myjournal.models import Name
+#from myjournal.models import Name
 
 # Register your models here.
 
 #class JournalAdmin(admin.ModelAdmin):
  #   list_display = ('j_date','j_time','j_title',)
-admin.register(Name)
-
-class NameAdmin(admin.ModelAdmin):
-    list_display = ('type','email','password',)
-
-admin.site.register(Name, NameAdmin)
