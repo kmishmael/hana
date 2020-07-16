f27c74a460da7f3a97b608b440aa6fe30f6c90d8
kibet ishmael <66499851+kmishmael@users.noreply.github.com>
2020-07-16 20:02:39 +0300

    Update settings.py

--- hana/settings.py
+++ hana/settings.py
@@ -166,6 +166,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+ #   os.path.join(BASE_DIR, 'static'),
+#)
